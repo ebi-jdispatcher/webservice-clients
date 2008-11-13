0a2aab90757d2805f564c0c1f1ee6794d75a37af
hpm <hpm@mycompany.com>
2008-11-13 12:48:43 +0000

    Working status, poll and submit.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@830 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

--- python/soappy/ncbiblast.py
+++ python/soappy/ncbiblast.py
@@ -9,11 +9,12 @@
 # http://www.ebi.ac.uk/Tools/webservices/tutorials/python
 # ======================================================================
 # WSDL URL for service
-wsdlUrl = ''
+wsdlUrl = 'http://wwwdev.ebi.ac.uk/Tools/jdispatcher/services/soap/ncbiblast?wsdl'
 
 # Load libraries
 import os
 import sys
+import base64
 import time
 from SOAPpy import WSDL
 from optparse import OptionParser
@@ -24,12 +25,27 @@ usage = "Usage: %prog [options...] [seqFile]"
 parser = OptionParser(usage=usage)
 # General options
 parser.add_option('--email', help='E-mail address')
+parser.add_option('--title', help='Job title')
 parser.add_option('--outfile', help='File name for results')
+parser.add_option('--outformat', help='Output format for results')
 parser.add_option('--polljob', action="store_true", help='Get job result')
 parser.add_option('--status', action="store_true", help='Get job status')
 parser.add_option('--jobid', help='Job identifier')
+parser.add_option('--async', action='store_true', help='Asynchronous mode')
 parser.add_option('--trace', action="store_true", help='Show SOAP messages')
 # Tool specific options
+parser.add_option('-p', '--program', help='Program to run')
+parser.add_option('-D', '--database', help='Database to search')
+parser.add_option('-m', '--matrix', help='Scoring matrix')
+parser.add_option('-E', '--exp', type='float', help='E-value threshold')
+parser.add_option('-f', '--filter', action="store_true", help='Low complexity sequence filter')
+parser.add_option('-n', '--numal', type='int', help='Maximum number of alignments')
+parser.add_option('-s', '--scores', type='int', help='Maximum number of scores')
+parser.add_option('-d', '--dropoff', type='int', help='Dropoff score')
+parser.add_option('--match_score', help='Match/missmatch score')
+parser.add_option('-o', '--opengap', type='int', help='Open gap penalty')
+parser.add_option('-x', '--extendgap', type='int', help='Extend gap penalty')
+parser.add_option('-g', '--gapalign', action="store_true", help='Optimise gap alignments')
 parser.add_option('--sequence', help='Input sequence file name')
 
 (options, args) = parser.parse_args()
@@ -42,11 +58,41 @@ if options.trace:
     server.soapproxy.config.dumpSOAPOut = 1
     server.soapproxy.config.dumpSOAPIn = 1
 
+# Get job status
+def serviceCheckStatus(jobId):
+    result = server.getStatus(jobId = jobId)
+    return result
+
+# Submit job
+def serviceRun(email, title, params):
+    jobid = server.run(email=email, title=title, parameters=params)
+    return jobid
+
+# Get available result types for job
+def serviceGetResultTypes(jobId):
+    result = server.getResultTypes(jobId=jobId)
+    return result['type']
+
+# Get result
+def serviceGetResult(jobId, type):
+    result = server.getRawResultOutput(jobId=jobId, type=type)
+    return base64.decodestring(result)
+
+# Get input parameters list
+def serviceGetParameters():
+    result = server.getParameters(tool='ncbiblast')
+    return result
+
+# Get input parameter information
+def serviceGetParameterDetails(paramName):
+    result= server.getParameterDetails(tool='ncbiblast', parameter=paramName)
+    return result
+
 # Client-side poll
 def clientPoll(jobId):
     result = 'PENDING'
     while result == 'RUNNING' or result == 'PENDING':
-        result = server.checkStatus(jobId)
+        result = serviceCheckStatus(jobId)
         print >>sys.stderr, result
         if result == 'RUNNING' or result == 'PENDING':
             time.sleep(15)
@@ -56,17 +102,17 @@ def getResult(jobId):
     # Check status and wait if necessary
     clientPoll(jobId)
     # Get available result types
-    resultTypes = server.getResults(jobId)
+    resultTypes = serviceGetResultTypes(jobId)
     for resultType in resultTypes:
         # Get the result
-        result = server.poll(jobId, resultType.type)
+        result = serviceGetResult(jobId, resultType)
         # Derive the filename for the result
         if options.outfile:
-            filename = options.outfile + '.' + resultType.ext
+            filename = options.outfile + '.' + resultType
         else:
-            filename = jobId + '.' + resultType.ext
+            filename = jobId + '.' + resultType
         # Write a result file
-        if not options.outformat or options.outformat == resultType.type:
+        if not options.outformat or options.outformat == resultType:
             fh = open(filename, 'w');
             fh.write(result)
             fh.close()
@@ -84,40 +130,52 @@ if options.polljob and options.jobid:
     getResult(options.jobid)
 # Get status
 elif options.status and options.jobid:
-    status = server.checkStatus(options.jobid)
+    status = serviceCheckStatus(options.jobid)
     print status
 # Submit job
 elif options.email and not options.jobid:
+    params = {}
     if len(args) > 0:
         if os.access(args[0], os.R_OK): # Read file into content
-            seqData = readFile(args[0])
-            content = [{'type':'sequence', 'content':seqData}]
+            params['sequence'] = readFile(args[0])
         else: # Argument is a sequence id
-            content = [{'type':'sequence', 'content':args[0]}]
+            params['sequence'] = args[0]
     elif options.sequence: # Specified via option
         if os.access(options.sequence, os.R_OK): # Read file into content
-            seqData = readFile(options.sequence)
-            content = [{'type':'sequence', 'content':seqData}]
+            params['sequence'] = readFile(options.sequence)
         else: # Argument is a sequence id
-            content = [{'type':'sequence', 'content':options.sequence}]
-    iprscan_params = {
-        'email':options.email,
-        'app':options.app,
-        'seqtype':options.seqtype,
-        'outformat':options.outformat
-        }
+            params['sequence'] = options.sequence
     # Booleans need to be represented as 1/0 rather than True/False
-    iprscan_params['async'] = 1
-    if options.crc:
-        iprscan_params['crc'] = 1
+    if options.gapalign:
+        params['gapalign'] = 1
     else:
-        iprscan_params['crc'] = 0
-    if options.goterms:
-        iprscan_params['goterms'] = 1
-    else:
-        iprscan_params['goterms'] = 0
+        params['gapalign'] = 0
+    # Add the other options (if defined)
+    if options.program:
+        params['program'] = options.program
+    if options.database:
+        params['database'] = options.database
+    if options.matrix:
+        params['matrix'] = options.matrix
+    if options.exp:
+        params['exp'] = options.exp
+    if options.filter:
+        params['filter'] = options.filter
+    if options.numal:
+        params['numal'] = options.numal
+    if options.scores:
+        params['scores'] = options.scores
+    if options.dropoff:
+        params['dropoff'] = options.dropoff
+    if options.match_score:
+        params['match_score'] = options.match_score
+    if options.opengap:
+        params['opengap'] = options.opengap
+    if options.extendgap:
+        params['extendgap'] = options.extendgap
+    
     # Submit the job
-    jobid = server.runInterProScan(params=iprscan_params, content=content)
+    jobid = serviceRun(options.email, options.title, params)
     if options.async: # Async mode
         print jobid
     else: # Sync mode
