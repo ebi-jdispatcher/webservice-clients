d188c615ce8d87c22ab8bd320c157156b199f9ac
hpm <hpm@mycompany.com>
2008-11-14 11:11:34 +0000

    Sumission, status, polling and result retrival working.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@835 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

--- python/zsi/ncbiblast_zsi_stubs.py
+++ python/zsi/ncbiblast_zsi_stubs.py
@@ -17,38 +17,40 @@ import sys
 import time
 from optparse import OptionParser
 # Load stubs
-from JDispatcherService_services import *
+from ncbiblast_services import *
 
 # Usage message
 usage = "%prog [options...] [seqFile]"
 
 # Process command-line options
 parser = OptionParser(usage=usage)
-parser.add_option('-p', '--program', help='Search program: blastp, blastn, blastx, etc.')
-parser.add_option('-D', '--database', help='Database to search')
-parser.add_option('-m', '--matrix', help='Scoring martix to use')
-parser.add_option('-E', '--exp', help='E-value threshold')
-parser.add_option('-f', '--filter', action="store_true", help='Low complexity filter')
-parser.add_option('-A', '--align', type='int', help='Number of alignments')
-parser.add_option('-s', '--scores', type='int', help='Number of scores')
-parser.add_option('-n', '--numal', type='int', help='Number of alignments')
-parser.add_option('-d', '--dropoff', type='int', help='Dropoff score')
-parser.add_option('-u', '--match', type='int', help='Match score')
-parser.add_option('-v', '--mismatch', type='int', help='Mismatch score')
-parser.add_option('-o', '--opengap', type='int', help='Open gap penalty')
-parser.add_option('-x', '--extendgap', type='int', help='Gap extension penality')
-parser.add_option('-g', '--gapalign', action="store_true", help='Optimise gap alignments')
-parser.add_option('--sequence', help='Input sequence file name')
-parser.add_option('--getParameters', help='Get parameter list')
-parser.add_option('-a', '--async', action="store_true", help='Async submission')
+# General options
 parser.add_option('--email', help='E-mail address')
 parser.add_option('--title', help='Job title')
-parser.add_option('-O', '--outfile', help='File name for results')
-parser.add_option('--outformat', help='Output format')
+parser.add_option('--outfile', help='File name for results')
+parser.add_option('--outformat', help='Output format for results')
 parser.add_option('--polljob', action="store_true", help='Get job result')
 parser.add_option('--status', action="store_true", help='Get job status')
-parser.add_option('-j', '--jobid', help='Job Id')
+parser.add_option('--jobid', help='Job identifier')
+parser.add_option('--async', action='store_true', help='Asynchronous mode')
 parser.add_option('--trace', action="store_true", help='Show SOAP messages')
+parser.add_option('--params', action="store_true", help='Get parameter list')
+parser.add_option('--paramDetail', help='Get parameter details')
+# Tool specific options
+parser.add_option('-p', '--program', help='Program to run')
+parser.add_option('-D', '--database', help='Database to search')
+parser.add_option('-m', '--matrix', help='Scoring matrix')
+parser.add_option('-E', '--exp', type='float', help='E-value threshold')
+parser.add_option('-f', '--filter', action="store_true", help='Low complexity sequence filter')
+parser.add_option('-n', '--numal', type='int', help='Maximum number of alignments')
+parser.add_option('-s', '--scores', type='int', help='Maximum number of scores')
+parser.add_option('-d', '--dropoff', type='int', help='Dropoff score')
+parser.add_option('--match_score', help='Match/missmatch score')
+parser.add_option('-o', '--opengap', type='int', help='Open gap penalty')
+parser.add_option('-x', '--extendgap', type='int', help='Extend gap penalty')
+parser.add_option('-g', '--gapalign', action="store_true", help='Optimise gap alignments')
+parser.add_option('--sequence', help='Input sequence file name')
+
 (options, args) = parser.parse_args()
 
 # Create the service interface (enabling trace if necessary)
@@ -66,6 +68,41 @@ def clientPoll(jobId):
     if statusStr == 'RUNNING' or statusStr == 'PENDING':
       time.sleep(15)
 
+# Submit a job
+def run(email, title, params):
+    req = runRequest()
+    req._email = email
+    req._title = title
+    req._parameters = ns0.InputParameters_Def
+    if params.has_key('program'):
+        req._parameters._program = params['program']
+    if params.has_key('database'):
+        req._parameters._database = params['database']
+    if params.has_key('matrix'):
+        req._parameters._matrix = params['matrix']
+    if params.has_key('exp'):
+        req._parameters._exp = params['exp']
+    if params.has_key('filter'):
+        req._parameters._filter = params['filter']
+    if params.has_key('numal'):
+        req._parameters._numal = params['numal']
+    if params.has_key('scores'):
+        req._parameters._scores = params['scores']
+    if params.has_key('dropoff'):
+        req._parameters._dropoff = params['dropoff']
+    if params.has_key('match_score'):
+        req._parameters._match_score = params['match_score']
+    if params.has_key('opengap'):
+        req._parameters._opengap = params['opengap']
+    if params.has_key('extendgap'):
+        req._parameters._extendgap = params['extendgap']
+    if params.has_key('gapalign'):
+        req._parameters._gapalign = params['gapalign']
+    if params.has_key('sequence'):
+        req._parameters._sequence = params['sequence']
+    msg = srvProxy.run(req)
+    return msg._jobId
+
 # Get status for a jobid
 def getStatus(jobid):
     req = getStatusRequest()
@@ -79,7 +116,7 @@ def getResultTypes(jobId):
     req = getResultTypesRequest()
     req._jobId = jobId
     msg = srvProxy.getResultTypes(req)
-    return msg._result
+    return msg._resultTypes._type
 
 # Get result of a specified type
 def getResultOutput(jobId, typeName):
@@ -87,21 +124,22 @@ def getResultOutput(jobId, typeName):
     req._jobId = jobId
     req._type = typeName
     msg = srvProxy.getRawResultOutput(req)
-    return msg._result
+    return msg._output
 
 # Get results for a jobid
 def getResults(jobId):
+    clientPoll(jobId)
     resultTypes = getResultTypes(jobId)
     for resultType in resultTypes:
         # Get the result
         result = getResultOutput(jobId, resultType)
         # Derive the filename for the result
         if options.outfile:
-            filename = options.outfile + '.' + resultType._ext
+            filename = options.outfile + '.' + resultType
         else:
-            filename = jobId + '.' + resultType._ext
+            filename = jobId + '.' + resultType
         # Write a result file
-        if not options.outformat or options.outformat == resultType.type:
+        if not options.outformat or options.outformat == resultType:
             fh = open(filename, 'w');
             fh.write(result)
             fh.close()
@@ -114,61 +152,62 @@ def readFile(filename):
     fh.close()
     return data
 
-if options.getParameters:
+if options.params:
     srvProxy.getParameters()
 # Get results
 elif options.polljob and options.jobid:
-    getResult(options.jobid)
+    getResults(options.jobid)
 # Get status
 elif options.status and options.jobid:
     print getStatus(options.jobid)
 # Submit job
 elif options.email and not options.jobid:
-    # Input data
+    params = {}
     if len(args) > 0:
         if os.access(args[0], os.R_OK): # Read file into content
-            seqData = readFile(args[0])
+            params['sequence'] = readFile(args[0])
         else: # Argument is a sequence id
-            seqData = args[0]
+            params['sequence'] = args[0]
     elif options.sequence: # Specified via option
         if os.access(options.sequence, os.R_OK): # Read file into content
-            seqData = readFile(options.sequence)
+            params['sequence'] = readFile(options.sequence)
         else: # Argument is a sequence id
-            seqData = options.sequence
-    params = {
-        'program':options.program,
-        'matrix':options.matrix,
-        'numal':options.numal,
-        'scores':options.scores,
-        'exp':options.exp,
-        'dropoff':options.dropoff,
-        #'match_scores':options.match,
-        'opengap':options.opengap,
-        'extendgap':options.extendgap,
-        'filter':options.filter,
-        #'seqrange':options.seqrange,
-        'align':options.align,
-        'sequence':seqData,
-        'database':options.database
-    }
+            params['sequence'] = options.sequence
     # Booleans need to be represented as 1/0 rather than True/False
-    #if options.gapalign:
-    #    params['gapalign'] = 1
-    #if options.format:
-    #    params['format'] = 1
+    if options.gapalign:
+        params['gapalign'] = 1
+    else:
+        params['gapalign'] = 0
+    # Add the other options (if defined)
+    if options.program:
+        params['program'] = options.program
+    if options.database:
+        params['database'] = options.database
+    if options.matrix:
+        params['matrix'] = options.matrix
+    if options.exp:
+        params['exp'] = options.exp
+    if options.filter:
+        params['filter'] = options.filter
+    if options.numal:
+        params['numal'] = options.numal
+    if options.scores:
+        params['scores'] = options.scores
+    if options.dropoff:
+        params['dropoff'] = options.dropoff
+    if options.match_score:
+        params['match_score'] = options.match_score
+    if options.opengap:
+        params['opengap'] = options.opengap
+    if options.extendgap:
+        params['extendgap'] = options.extendgap
     # Submit the job
-    runParams = {
-                  'email':options.email,
-                  'title':options.title,
-                  'parameters':params
-                  }
-    jobidDict = srvProxy.run(email=options.email, parameters=params)
-    jobid = jobidDict['jobid']
+    jobid = run(options.email, options.title, params)
     if options.async: # Async mode
         print jobid
-    #else: # Sync mode
-    #    time.sleep(1)
-    #    getResult(jobid)
+    else: # Sync mode
+        time.sleep(1)
+        getResults(jobid)
 else:
     print 'Error: unrecognised argument combination'
     parser.print_help()
