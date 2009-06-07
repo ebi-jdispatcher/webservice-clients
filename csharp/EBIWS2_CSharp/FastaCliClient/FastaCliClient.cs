943ec9980e81644e5db15bbd5bf33130251c27b3
hpm <hpm@mycompany.com>
2009-06-07 09:56:30 +0000

    Add outline projects for FASTA and WU-BLAST command-line clients.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@1048 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

---
+++ csharp/EBIWS2_CSharp/FastaCliClient/FastaCliClient.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FastaCliClient
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ csharp/EBIWS2_CSharp/WuBlastCliClient/WuBlastCliClient.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WuBlastCliClient
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
