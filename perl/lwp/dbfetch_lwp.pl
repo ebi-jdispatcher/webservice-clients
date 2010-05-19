e8910f28fa552536b2a5bc4f4a4d1fca596d6ae5
hpm <hpm@mycompany.com>
2010-05-19 14:14:29 +0000

    Enable Id tag
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@1536 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

--- perl/lwp/dbfetch_lwp.pl
+++ perl/lwp/dbfetch_lwp.pl
@@ -148,7 +148,7 @@ sub rest_request {
 
 	# Create a user agent
 	my $ua = LWP::UserAgent->new();
-	'$Revision: 1317 $' =~ m/(\d+)/;
+	'$Revision$' =~ m/(\d+)/;
 	$ua->agent("EBI-Sample-Client/$1 ($scriptName; $OSNAME) " . $ua->agent()
 );
 	$ua->env_proxy;
