6dc13aaab5a77317171abc34822a09d6695111cf
hpm <hpm@mycompany.com>
2010-05-19 08:18:19 +0000

    WSDbfetch document/literal SOAP service sample client.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@1525 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

---
+++ csharp/EBIWS2_CSharp/EbiWS/WSDbfetchClient.cs
@@ -0,0 +1,475 @@
+/* $Id$
+ * ======================================================================
+ * WSDbfetch web services C# client.
+ *
+ * See:
+ * http://www.ebi.ac.uk/Tools/webservices/services/dbfetch
+ * http://www.ebi.ac.uk/Tools/webservices/tutorials/csharp
+ * ====================================================================== */
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using EbiWS.WSDbfetchWs;
+
+namespace EbiWS {
+	/// <summary>WSDbfetch web services C# client.</summary>
+	public class WSDbfetchClient {
+		/// <value>
+		/// Level of output produced. Used to implment --quiet and --verbose.
+		/// </value>
+		public int OutputLevel {
+			get{return outputLevel;}
+			set{
+				if(value > -1) outputLevel = value;
+			}
+		}
+		private int outputLevel = 1;
+		/// <value>
+		/// Level of debug output (default off).
+		/// </value>
+		public int DebugLevel {
+			get{return debugLevel;}
+			set{
+				if(value > -1) debugLevel = value;
+			}
+		}
+		private int debugLevel = 0;
+		/// <value>
+		/// Specified endpoint for the SOAP service. If null the default 
+		/// endpoint specified in the WSDL (and thus in the generated 
+		/// stubs) is used.
+		/// </value>
+		public string ServiceEndPoint {
+			get{return serviceEndPoint;}
+			set{serviceEndPoint = value;}
+		}
+		private string serviceEndPoint = null;
+		/// <summary>Webservice proxy object</summary>
+		public WSDBFetchDoclitServerService SrvProxy
+		{
+			get { return srvProxy; }
+			set { srvProxy = value; }
+		}
+		private WSDBFetchDoclitServerService srvProxy = null;
+		
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public WSDbfetchClient()
+		{
+			OutputLevel = 1; // Normal output
+			DebugLevel = 0; // Debug output off.
+		}
+		
+		/// <summary>
+		/// Print a debug message at the specified level.
+		/// </summary>
+		/// <param name="methodName">Method name to use in output.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="message">Message to output.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="level">Debug level at which to output.
+		/// A <see cref="System.Int32"/>
+		/// </param>
+		protected void PrintDebugMessage(string methodName, string message, int level) {
+			if(level <= DebugLevel) Console.Error.WriteLine("[{0}()] {1}", methodName, message);
+		}
+
+		/// <summary>
+		/// Construct a string of the values of an object, both fields and properties.
+		/// </summary>
+		/// <param name="obj">
+		/// Object to get values from. A <see cref="System.Object"/>
+		/// </param>
+		/// <returns>
+		/// Name and the contents of values as a string. A <see cref="System.String"/>
+		/// </returns>
+		protected string ObjectValueToString(Object obj)
+		{
+			PrintDebugMessage("ObjectValueToString", "Begin", 31);
+			StringBuilder strBuilder = new StringBuilder();
+			strBuilder.Append(ObjectFieldsToString(obj));
+			strBuilder.Append(ObjectPropertiesToString(obj));
+			PrintDebugMessage("ObjectValueToString", "End", 31);
+			return strBuilder.ToString();
+		}
+
+		/// <summary>
+		/// Construct a string of the fields of an object.
+		/// </summary>
+		/// <param name="obj">
+		/// Object to get fields from. A <see cref="System.Object"/>
+		/// </param>
+		/// <returns>
+		/// Name and the contents of fields as a string. A <see cref="System.String"/>
+		/// </returns>
+		protected string ObjectFieldsToString(Object obj) {
+			PrintDebugMessage("ObjectFieldsToString", "Begin", 32);
+			StringBuilder strBuilder = new StringBuilder();
+			Type objType = obj.GetType();
+			PrintDebugMessage("ObjectFieldsToString", "objType: " + objType, 33);
+			foreach(System.Reflection.FieldInfo info in objType.GetFields()) {
+				PrintDebugMessage("ObjectFieldsToString", "info: " + info.Name, 33);
+				if (info.FieldType.IsArray)
+				{
+					strBuilder.Append(info.Name + ":\n");
+					foreach(Object subObj in (Object[])info.GetValue(obj)) {
+						strBuilder.Append("\t" + subObj);
+					}
+				}
+				else {
+					strBuilder.Append(info.Name + ": " + info.GetValue(obj) + "\n");
+				}
+			}
+			PrintDebugMessage("ObjectFieldsToString", "End", 32);
+			return strBuilder.ToString();
+		}
+		
+		/// <summary>
+		/// Construct a string of the properties of an object.
+		/// </summary>
+		/// <param name="obj">
+		/// Object to get properties from. A <see cref="System.Object"/>
+		/// </param>
+		/// <returns>
+		/// Name and the contents of properties as a string. A <see cref="System.String"/>
+		/// </returns>
+		protected string ObjectPropertiesToString(Object obj)
+		{
+			PrintDebugMessage("ObjectPropertiesToString", "Begin", 31);
+			StringBuilder strBuilder = new StringBuilder();
+			Type objType = obj.GetType();
+			PrintDebugMessage("ObjectPropertiesToString", "objType: " + objType, 32);
+			foreach (PropertyInfo info in objType.GetProperties())
+			{
+				PrintDebugMessage("ObjectPropertiesToString", "info: " + info.Name, 32);
+				if (info.PropertyType.IsArray)
+				{
+					strBuilder.Append(info.Name + ":\n");
+					foreach (Object subObj in (Object[])info.GetValue(obj, null))
+					{
+						strBuilder.Append("\t" + subObj);
+					}
+				}
+				else
+				{
+					strBuilder.Append(info.Name + ": " + info.GetValue(obj, null) + "\n");
+				}
+			}
+			PrintDebugMessage("ObjectPropertiesToString", "End", 31);
+			return strBuilder.ToString();
+		}
+		
+		/// <summary>
+		/// Print a progress message, at the specified output level.
+		/// </summary>
+		/// <param name="msg">Message to print.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="level">Output level at which to print the message.
+		/// A <see cref="System.Int32"/>
+		/// </param>
+		protected void PrintProgressMessage(String msg, int level) {
+			if(OutputLevel >= level) Console.Error.WriteLine(msg);
+		}
+
+		/// <summary>
+		/// Get the service connection. Has to be called before attempting to use any of the service operations.
+		/// </summary>
+		protected void ServiceProxyConnect()
+		{
+			PrintDebugMessage("ServiceProxyConnect", "Begin", 11);
+			if (SrvProxy == null) {
+				if(ServiceEndPoint == null) {
+					SrvProxy = new WSDBFetchDoclitServerService();
+				}
+				else {
+					SrvProxy = new WSDBFetchDoclitServerService();
+					SrvProxy.Url = ServiceEndPoint;
+				}
+				ServiceEndPoint = SrvProxy.Url;
+				PrintDebugMessage("ServiceProxyConnect", "ServiceEndPoint: " + ServiceEndPoint, 12);
+				PrintDebugMessage("ServiceProxyConnect", "SrvProxy: " + SrvProxy, 12);
+			}
+			PrintDebugMessage("ServiceProxyConnect", "End", 11);
+		}
+
+		
+		/// <summary>
+		/// Get list of database names from sevice.
+		/// </summary>
+		/// <returns>An array of database names.
+		/// A <see cref="System.String"/>
+		/// </returns>
+		public string[] GetSupportedDBs()
+		{
+			PrintDebugMessage("GetSupportedDBs", "Begin", 1);
+			ServiceProxyConnect();
+			string[] dbNameList = SrvProxy.getSupportedDBs();
+			PrintDebugMessage("GetSupportedDBs", "got " + dbNameList.Length + " db names", 2);
+			PrintDebugMessage("GetSupportedDBs", "End", 1);
+			return dbNameList;
+		}
+		
+		/// <summary>
+		/// Print list of available search databases.
+		/// </summary>
+		public void PrintGetSupportedDBs() {
+			PrintDebugMessage("PrintGetSupportedDBs", "Begin", 1);
+			string[] result = GetSupportedDBs();
+			PrintStrList(result);
+			PrintDebugMessage("PrintGetSupportedDBs", "End", 1);
+		}
+		
+		/// <summary>
+		/// Get list of database and format names from sevice.
+		/// </summary>
+		/// <returns>An array of database and format names.
+		/// A <see cref="System.String"/>
+		/// </returns>
+		public string[] GetSupportedFormats()
+		{
+			PrintDebugMessage("GetSupportedFormats", "Begin", 1);
+			ServiceProxyConnect();
+			string[] nameList = SrvProxy.getSupportedFormats();
+			PrintDebugMessage("GetSupportedFormats", "got " + nameList.Length + " names", 2);
+			PrintDebugMessage("GetSupportedFormats", "End", 1);
+			return nameList;
+		}
+		
+		/// <summary>
+		/// Print list of available search databases and formats.
+		/// </summary>
+		public void PrintGetSupportedFormats() {
+			PrintDebugMessage("PrintGetSupportedFormats", "Begin", 1);
+			string[] result = GetSupportedFormats();
+			PrintStrList(result);
+			PrintDebugMessage("PrintGetSupportedFormats", "End", 1);
+		}
+		
+		/// <summary>
+		/// Get list of database and style names from sevice.
+		/// </summary>
+		/// <returns>An array of database and style names.
+		/// A <see cref="System.String"/>
+		/// </returns>
+		public string[] GetSupportedStyles()
+		{
+			PrintDebugMessage("GetSupportedStyles", "Begin", 1);
+			ServiceProxyConnect();
+			string[] nameList = SrvProxy.getSupportedStyles();
+			PrintDebugMessage("GetSupportedStyles", "got " + nameList.Length + " names", 2);
+			return nameList;
+		}
+		
+		/// <summary>
+		/// Print list of available search databases and styles.
+		/// </summary>
+		public void PrintGetSupportedStyles() {
+			PrintDebugMessage("PrintGetSupportedStyles", "Begin", 1);
+			string[] result = GetSupportedStyles();
+			PrintStrList(result);
+			PrintDebugMessage("PrintGetSupportedStyles", "End", 1);
+		}
+		
+		/// <summary>
+		/// Get list of format names for a database.
+		/// </summary>
+		/// <param name="dbName">
+		/// Database name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <returns>An array of format names.
+		/// A <see cref="System.String"/>
+		/// </returns>
+		public string[] GetDbFormats(string dbName)
+		{
+			PrintDebugMessage("GetDbFormats", "Begin", 1);
+			ServiceProxyConnect();
+			string[] nameList = SrvProxy.getDbFormats(dbName);
+			PrintDebugMessage("GetDbFormats", "got " + nameList.Length + " names", 2);
+			PrintDebugMessage("GetDbFormats", "End", 1);
+			return nameList;
+		}
+		
+		/// <summary>
+		/// Print list of available format names for a database.
+		/// </summary>
+		/// <param name="dbName">
+		/// Database name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		public void PrintGetDbFormats(string dbName) {
+			PrintDebugMessage("PrintGetDbFormats", "Begin", 1);
+			string[] result = GetDbFormats(dbName);
+			PrintStrList(result);
+			PrintDebugMessage("PrintGetDbFormats", "End", 1);
+		}
+		
+		/// <summary>
+		/// Get list of style names for a format of a database.
+		/// </summary>
+		/// <param name="dbName">
+		/// Database name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="formatName">
+		/// Data format name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <returns>An array of style names.
+		/// A <see cref="System.String"/>
+		/// </returns>
+		public string[] GetFormatStyles(string dbName, string formatName)
+		{
+			PrintDebugMessage("GetFormatStyles", "Begin", 1);
+			ServiceProxyConnect();
+			string[] nameList = SrvProxy.getFormatStyles(dbName, formatName);
+			PrintDebugMessage("GetFormatStyles", "got " + nameList.Length + " names", 2);
+			PrintDebugMessage("GetFormatStyles", "End", 1);
+			return nameList;
+		}
+		
+		/// <summary>
+		/// Print list of available style names for a format of a database.
+		/// </summary>
+		/// <param name="dbName">
+		/// Database name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="formatName">
+		/// Data format name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		public void PrintGetFormatStyles(string dbName, string formatName) {
+			PrintDebugMessage("PrintGetFormatStyles", "Begin", 1);
+			string[] result = GetFormatStyles(dbName, formatName);
+			PrintStrList(result);
+			PrintDebugMessage("PrintGetFormatStyles", "End", 1);
+		}
+		
+		/// <summary>
+		/// Fetch an entry.
+		/// </summary>
+		/// <param name="query">
+		/// Entry identifier in DB:ID format.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="formatName">
+		/// Data format name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="styleName">
+		/// Result style name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <returns>Entry data.
+		/// A <see cref="System.String"/>
+		/// </returns>
+		public string FetchData(string query, string formatName, string styleName)
+		{
+			PrintDebugMessage("FetchData", "Begin", 1);
+			ServiceProxyConnect();
+			string entryStr = SrvProxy.fetchData(query, formatName, styleName);
+			PrintDebugMessage("FetchData", "End", 1);
+			return entryStr;
+		}
+		
+		/// <summary>
+		/// Print an entry.
+		/// </summary>
+		/// <param name="query">
+		/// Entry identifier in DB:ID format.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="formatName">
+		/// Data format name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="styleName">
+		/// Result style name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		public void PrintFetchData(string query, string formatName, string styleName) {
+			PrintDebugMessage("PrintFetchData", "Begin", 1);
+			string result = FetchData(query, formatName, styleName);
+			Console.WriteLine(result);
+			PrintDebugMessage("PrintFetchData", "End", 1);
+		}
+		
+		/// <summary>
+		/// Fetch a set of entries.
+		/// </summary>
+		/// <param name="dbName">
+		/// Database name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="idListStr">
+		/// Comma or space separated list of entry identifiers.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="formatName">
+		/// Data format name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="styleName">
+		/// Result style name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <returns>Entries data.
+		/// A <see cref="System.String"/>
+		/// </returns>
+		public string FetchBatch(string dbName, string idListStr, string formatName, string styleName)
+		{
+			PrintDebugMessage("FetchBatch", "Begin", 1);
+			ServiceProxyConnect();
+			string entriesStr = SrvProxy.fetchBatch(dbName, idListStr, formatName, styleName);
+			PrintDebugMessage("FetchBatch", "End", 1);
+			return entriesStr;
+		}
+		
+		/// <summary>
+		/// Print a set of entries.
+		/// </summary>
+		/// <param name="dbName">
+		/// Database name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="idListStr">
+		/// Comma or space separated list of entry identifiers.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="formatName">
+		/// Data format name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="styleName">
+		/// Result style name.
+		/// A <see cref="System.String"/>
+		/// </param>
+		public void PrintFetchBatch(string dbName, string idListStr, string formatName, string styleName) {
+			PrintDebugMessage("PrintFetchBatch", "Begin", 1);
+			string result = FetchBatch(dbName, idListStr, formatName, styleName);
+			Console.WriteLine(result);
+			PrintDebugMessage("PrintFetchBatch", "End", 1);
+		}
+		
+		/// <summary>
+		/// Print an array of strings as a list.
+		/// </summary>
+		/// <param name="strList">
+		/// Array of string to print.
+		/// A <see cref="System.String"/>
+		/// </param>
+		private void PrintStrList(string[] strList) {
+			foreach (string item in strList) {
+				if (item != null && item != "") {
+					Console.WriteLine(item);
+				}
+			}
+		}
+	}
+}
---
+++ csharp/EBIWS2_CSharp/WSDbfetchCliClient/WSDbfetchCliClient.cs
@@ -0,0 +1,180 @@
+/* $Id$
+ * ======================================================================
+ * SOAP command-line client for WSDbfetch.
+ * ====================================================================== */
+using System;
+using System.IO;
+using EbiWS.WSDbfetchWs;
+
+namespace EbiWS
+{
+	class WSDbfetchCliClient : EbiWS.WSDbfetchClient
+	{
+		/// <summary>Tool specific usage</summary>
+		private string usageMsg = @"WSDbfetch
+======
+
+WSDbfetchCliClient.exe <method> [arguments...]
+
+A number of methods are available:
+
+getSupportedDBs - list available databases
+getSupportedFormats - list available databases with formats
+getSupportedStyles - list available databases with styles
+getDbFormats - list formats for a specifed database
+getFormatStyles - list styles for a specified database and format
+fetchData - retrive an database entry. See below for details of arguments.
+fetchBatch - retrive database entries. See below for details of arguments.
+
+Fetching an entry: fetchData
+
+WSDbfetchCliClient.exe fetchData <dbName:id> [format [style]]
+
+dbName:id  database name and entry ID or accession (e.g. UNIPROT:WAP_RAT)
+format     format to retrive (e.g. uniprot)
+style      style to retrive (e.g. raw)
+
+Fetching entries: fetchBatch
+
+WSDbfetchCliClient.exe fetchBatch <dbName> <idList> [format [style]]
+
+dbName     database name (e.g. UNIPROT)
+idList     list of entry IDs or accessions (e.g. 1433T_RAT,WAP_RAT).
+           Maximum of 200 IDs or accessions.
+format     format to retrive (e.g. uniprot)
+style      style to retrive (e.g. raw)
+
+Further information:
+
+  http://www.ebi.ac.uk/Tools/webservices/services/dbfetch
+  http://www.ebi.ac.uk/Tools/webservices/tutorials/csharp
+
+Support/Feedback:
+
+  http://www.ebi.ac.uk/support/
+";
+
+		/// <summary>Execution entry point</summary>
+		/// <param name="args">Command-line parameters</param>
+		/// <returns>Exit status</returns>
+		public static int Main(string[] args)
+		{
+			int retVal = 0; // Return value
+			// Create an instance of the wrapper object
+			WSDbfetchCliClient wsApp = new WSDbfetchCliClient();
+			// If no arguments print usage and return
+			if (args.Length < 1)
+			{
+				wsApp.PrintUsageMessage();
+				return retVal;
+			}
+			try
+			{
+				// Parse the command line
+				retVal = wsApp.ParseCommand(args);
+			}
+			catch (System.Exception ex)
+			{ // Catch all exceptions
+				Console.Error.WriteLine("Error: " + ex.Message);
+				Console.Error.WriteLine(ex.StackTrace);
+				retVal = 2;
+			}
+			return retVal;
+		}
+
+		/// <summary>
+		/// Print the usage message.
+		/// </summary>
+		private void PrintUsageMessage()
+		{
+			PrintDebugMessage("PrintUsageMessage", "Begin", 1);
+			Console.WriteLine(usageMsg);
+			PrintDebugMessage("PrintUsageMessage", "End", 1);
+		}
+
+		/// <summary>Parse command-line options</summary>
+		/// <param name="args">Command-line options</param>
+		private int ParseCommand(string[] args)
+		{
+			PrintDebugMessage("ParseCommand", "Begin", 1);
+			// Return value
+			int retVal = 0;
+			string formatName = "default";
+			string styleName = "default";
+			// Loop over command-line options
+			for (int i = 0; (retVal == 0 && i < args.Length); i++)
+			{
+				PrintDebugMessage("parseCommand", "arg: " + args[i], 2);
+				switch (args[i])
+				{
+						// Generic options
+					case "--help": // Usage info
+						this.PrintUsageMessage();
+						break;
+					case "-h":
+						goto case "--help";
+					case "/help":
+						goto case "--help";
+					case "/h":
+						goto case "--help";
+					case "--verbose": // Output level
+						OutputLevel++;
+						break;
+					case "/verbose":
+						goto case "--verbose";
+					case "--quiet": // Output level
+						OutputLevel--;
+						break;
+					case "/quiet":
+						goto case "--quiet";
+					case "--debugLevel":
+						DebugLevel = Convert.ToInt32(args[++i]);
+						break;
+					case "/debugLevel":
+						goto case "--debugLevel";
+					case "--endpoint": // Service endpoint
+						ServiceEndPoint = args[++i];
+						break;
+					case "/endpoint":
+						goto case "--endpoint";
+					
+					case "getSupportedDBs": // Databases available to search
+						this.PrintGetSupportedDBs();
+						break;
+					case "getSupportedFormats": // Databases and formats available
+						this.PrintGetSupportedFormats();
+						break;
+					case "getSupportedStyles": // Databases and styles available
+						this.PrintGetSupportedStyles();
+						break;
+					case "getDbFormats": // Formats for a database.
+						this.PrintGetDbFormats(args[++i]);
+						break;
+					case "getFormatStyles": // Styles for a format of a database.
+						this.PrintGetFormatStyles(args[++i], args[++i]);
+						break;
+					case "fetchData": // Fetch an entry.
+						string query = args[++i];
+						if(args.Length > (i + 1)) formatName = args[++i];
+						if(args.Length > (i + 1)) styleName = args[++i];
+						this.PrintFetchData(query, formatName, styleName);
+						break;
+					case "fetchBatch": // Fetch a set of entries.
+						string dbName = args[++i];
+						string idListStr = args[++i];
+						if(args.Length > (i + 1)) formatName = args[++i];
+						if(args.Length > (i + 1)) styleName = args[++i];
+						this.PrintFetchBatch(dbName, idListStr, formatName, styleName);
+						break;
+
+					default: // Don't know what to do, so print error message
+						Console.Error.WriteLine("Error: unknown option: " + args[i] + "\n");
+						retVal = 1;
+						break;
+				}
+			}
+			PrintDebugMessage("ParseCommand", "End", 1);
+			return retVal;
+		}
+    }
+}
