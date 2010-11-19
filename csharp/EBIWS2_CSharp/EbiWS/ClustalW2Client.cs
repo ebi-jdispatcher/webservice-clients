914c608ea364af448609ea5bc09a07f02b7c0e49
hpm <hpm@mycompany.com>
2010-11-19 15:09:00 +0000

    Tweak user-agent string for Mono and MS .NET.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@1635 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

--- csharp/EBIWS2_CSharp/EbiWS/ClustalW2Client.cs
+++ csharp/EBIWS2_CSharp/EbiWS/ClustalW2Client.cs
@@ -62,7 +62,13 @@ namespace EbiWS
 		private void SetProxyUserAgent() {
 			PrintDebugMessage("SetProxyUserAgent", "Begin", 11);
 			String clientVersion = revision.Substring(11, (revision.Length - 13));
-			String userAgent = "EBI-Sample-Client/" + clientVersion + " (" + this.GetType().Name + "; " + System.Environment.OSVersion.Platform.ToString() + "; " + SrvProxy.UserAgent + ")";
+			String userAgent = "EBI-Sample-Client/" + clientVersion + " (" + this.GetType().Name + "; " + System.Environment.OSVersion.ToString();
+			if(SrvProxy.UserAgent.Contains("(")) { // MS .NET
+				userAgent += ") " + SrvProxy.UserAgent;
+			}
+			else { // Mono
+				userAgent += "; " + SrvProxy.UserAgent + ")";
+			}
 			PrintDebugMessage("SetProxyUserAgent", "userAgent: " + userAgent, 12);
 			SrvProxy.UserAgent = userAgent;
 			PrintDebugMessage("SetProxyUserAgent", "End", 11);
