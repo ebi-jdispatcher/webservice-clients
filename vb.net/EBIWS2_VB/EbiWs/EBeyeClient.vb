294a3ec41cd3613c0a74e2f36fc570c04db8e515
hpm <hpm@mycompany.com>
2011-08-27 09:50:42 +0000

    Add stubs for EB-eye and InterProScan.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@1962 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

---
+++ vb.net/EBIWS2_VB/EbiWs/EBeyeClient.vb
@@ -0,0 +1,14 @@
+
+Option Explicit On
+Option Strict On
+
+Imports System
+
+
+
+Public Class EBeyeClient
+	
+	Public Sub New()
+		MyBase.New
+	End Sub
+End Class
---
+++ vb.net/EBIWS2_VB/EbiWs/IPRScanClient.vb
@@ -0,0 +1,14 @@
+
+Option Explicit On
+Option Strict On
+
+Imports System
+
+
+
+Public Class IPRScanClient
+	
+	Public Sub New()
+		MyBase.New
+	End Sub
+End Class
