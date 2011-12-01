f5203f198e75b81a25c1f5da3df6e070f5d80182
wli <wli@mycompany.com>
2011-12-01 11:23:40 +0000

    Fixed description
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@2047 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

--- perl/lwp/phobius_lwp.pl
+++ perl/lwp/phobius_lwp.pl
@@ -953,7 +953,7 @@ Asynchronous job:
 
 Further information:
 
-  http://www.ebi.ac.uk/Tools/webservices/services/sss/phobius_rest
+  http://www.ebi.ac.uk/Tools/webservices/services/pfa/phobius_rest
   http://www.ebi.ac.uk/Tools/webservices/tutorials/perl
 
 Support/Feedback:
