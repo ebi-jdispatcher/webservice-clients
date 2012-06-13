0f0c5ec1dfe615c699063443dfa5d7f0dd102da5
hpm <hpm@mycompany.com>
2012-06-13 13:41:21 +0000

    Return an appropriate error code so make failes on errors. Add noe ebout implementing file support for input sequence.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@2126 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

--- ruby/soap4r/ncbiblast_soap4r.rb
+++ ruby/soap4r/ncbiblast_soap4r.rb
@@ -498,6 +498,7 @@ begin
 
   # Submit a job
   elsif(ARGV[0] || argHash['sequence'])
+    # TODO: implement input sequence fetch from file.
     if(ARGV[0])
       params.sequence = ARGV[0]
     end
@@ -531,4 +532,5 @@ begin
 rescue StandardError => ex
   $stderr.puts 'Exception'
   $stderr.puts ex
+  exit(2)
 end
