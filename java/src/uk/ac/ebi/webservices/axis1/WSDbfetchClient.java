d45546b334b2474b0159454ac6236e96aba2895f
hpm <hpm@mycompany.com>
2013-01-25 12:35:31 +0000

    Add license text.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@2459 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

--- java/src/uk/ac/ebi/webservices/axis1/WSDbfetchClient.java
+++ java/src/uk/ac/ebi/webservices/axis1/WSDbfetchClient.java
@@ -1,4 +1,26 @@
-/* $Id$ */
+/* $Id$
+ * ======================================================================
+ * 
+ * Copyright 2010-2013 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ======================================================================
+ * WSDbfetch web service Java client using Axis 1.x.
+ * ----------------------------------------------------------------------
+ * Tested with:
+ *   Sun Java 1.5.0_17 with Apache Axis 1.4 on CentOS 5.2.
+ * ====================================================================== */
 package uk.ac.ebi.webservices.axis1;
 
 import java.io.BufferedReader;
