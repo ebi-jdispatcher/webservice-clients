cd7f97006fe74f1d54437f37dd386646840c3080
hpm <hpm@mycompany.com>
2013-02-05 11:14:28 +0000

    Initial SeqCksum sample client.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@2502 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

---
+++ csharp/EBIWS2_CSharp/EbiWS/SeqCksumClient.cs
@@ -0,0 +1,380 @@
+/* $Id$
+ * ======================================================================
+ * 
+ * Copyright 2009-2013 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ======================================================================
+ * JDispatcher SOAP client for SeqCksum
+ * 
+ * See:
+ * http://www.ebi.ac.uk/Tools/webservices/services/so/seqcksum_soap
+ * http://www.ebi.ac.uk/Tools/webservices/tutorials/csharp
+ * ====================================================================== */
+using System;
+using System.IO;
+using EbiWS.SeqCksumWs; // "Web Reference" or wsdl.exe generated stubs.
+
+namespace EbiWS
+{
+	/// <summary>
+	/// Client for EMBL-EBI SeqCksum SOAP web service.
+	/// </summary>
+	public class SeqCksumClient : EbiWS.AbstractWsClient
+	{
+		/// <summary>Webservice proxy object</summary>
+		public JDispatcherService SrvProxy
+		{
+			get { return srvProxy; }
+			set { srvProxy = value; }
+		}
+		private JDispatcherService srvProxy = null;
+		/// <summary>Parameters used for launching jobs</summary>
+		public InputParameters InParams
+		{
+			get { return inParams; }
+			set { inParams = value; }
+		}
+		private InputParameters inParams = null;
+		/// <summary>Multiple fasta formatted sequences as input.</summary>
+		protected Boolean multifasta = false;
+		// Client object revision.
+		private string revision = "$Revision$";
+		
+		// Default constructor. Required for abstract class constructor.
+		public SeqCksumClient()
+		{
+		}
+
+		// Implementation of abstract method (AbsractWsClient.ServiceProxyConnect()).
+		protected override void ServiceProxyConnect()
+		{
+			PrintDebugMessage("ServiceProxyConnect", "Begin", 11);
+			if (SrvProxy == null) {
+				SrvProxy = new JDispatcherService();
+				SetProxyEndPoint(); // Set explicit service endpoint, if defined.
+				SetProxyUserAgent(); // Set user-agent for client.
+				SrvProxy.EnableDecompression = true; // Enable response compression.
+			}
+			PrintDebugMessage("ServiceProxyConnect", "SrvProxy: " + SrvProxy.ToString(), 12);
+			PrintDebugMessage("ServiceProxyConnect", "End", 11);
+		}
+		
+		// Set service proxy endpoint.
+		private void SetProxyEndPoint() {
+			PrintDebugMessage("SetProxyEndPoint", "Begin", 11);
+			if(ServiceEndPoint != null && ServiceEndPoint.Length > 0) {
+				SrvProxy.Url = ServiceEndPoint;
+			}
+			ServiceEndPoint = SrvProxy.Url;
+			PrintDebugMessage("SetProxyEndPoint", "Service endpoint: " + SrvProxy.Url, 12);
+			PrintDebugMessage("SetProxyEndPoint", "End", 11);
+		}
+
+		// Set User-agent for web service proxy.
+		private void SetProxyUserAgent() {
+			PrintDebugMessage("SetProxyUserAgent", "Begin", 11);
+			String userAgent = ConstuctUserAgentStr(revision, this.GetType().Name, SrvProxy.UserAgent);
+			PrintDebugMessage("SetProxyUserAgent", "userAgent: " + userAgent, 12);
+			SrvProxy.UserAgent = userAgent;
+			PrintDebugMessage("SetProxyUserAgent", "End", 11);
+		}
+		
+		// Implementation of abstract method (AbsractWsClient.GetParams()).
+		public override string[] GetParams()
+		{
+			PrintDebugMessage("GetParams", "Begin", 1);
+			ServiceProxyConnect();
+			string[] paramNameList = SrvProxy.getParameters();
+			PrintDebugMessage("GetParams", "got " + paramNameList.Length + " parameter names", 2);
+			PrintDebugMessage("GetParams", "End", 1);
+			return paramNameList;
+		}
+
+		/// <summary>
+		/// Get detailed information about a named parameter. 
+		/// </summary>
+		/// <param name="paramName">Name of the parameter to get the detailed description of.
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <returns>Object describing the parameter.
+		/// A <see cref="wsParameterDetails"/>
+		/// </returns>
+		public wsParameterDetails GetParamDetail(string paramName)
+		{
+			PrintDebugMessage("GetParamDetail", "Begin", 1);
+			PrintDebugMessage("GetParamDetail", "paramName: " + paramName, 2);
+			ServiceProxyConnect();
+			wsParameterDetails paramDetail = SrvProxy.getParameterDetails(paramName);
+			PrintDebugMessage("GetParamDetail", "End", 1);
+			return paramDetail;
+		}
+
+		// Implementation of abstract method (AbsractWsClient.PrintParammDetail()).
+		protected override void PrintParamDetail(string paramName)
+		{
+			PrintDebugMessage("PrintParamDetail", "Begin", 1);
+			wsParameterDetails paramDetail = GetParamDetail(paramName);
+			Console.WriteLine("{0}\t{1}", paramDetail.name, paramDetail.type);
+			if (paramDetail.description != null) Console.WriteLine(paramDetail.description);
+			if (paramDetail.values != null)
+			{
+				foreach (wsParameterValue paramValue in paramDetail.values)
+				{
+					Console.Write(paramValue.value);
+					if (paramValue.defaultValue) Console.Write("\tdefault");
+					Console.WriteLine();
+					if (paramValue.label != null) Console.WriteLine("\t{0}", paramValue.label);
+					if (paramValue.properties != null)
+					{
+						foreach (wsProperty valueProperty in paramValue.properties)
+						{
+							Console.WriteLine("\t{0}\t{1}", valueProperty.key, valueProperty.value);
+						}
+					}
+				}
+			}
+			PrintDebugMessage("PrintParamDetail", "End", 1);
+		}
+
+		/// <summary>Submit job(s) to the service.</summary>
+		protected void SubmitJobs() {
+			PrintDebugMessage("SubmitJobs", "Begin", 1);
+			// Three modes...
+			// 1. Multiple fasta sequence input.
+			if(this.multifasta) {
+				SetSequenceFile(InParams.sequence);
+				string inSeq = null;
+				while((inSeq = NextSequence()) != null) {
+					InParams.sequence = inSeq;
+					SubmitJob();
+				}
+				CloseSequenceFile();
+			}
+			// 2. Entry identifier list input.
+			else if(InParams.sequence.StartsWith("@")) {
+				SetIdentifierFile(InParams.sequence.Substring(1));
+				string inId = null;
+				while((inId = NextIdentifier()) != null) {
+					InParams.sequence = inId;
+					SubmitJob();
+				}
+				CloseIdentifierFile();
+			}
+			// 3. Simple sequence input.
+			else {
+				InParams.sequence = LoadData(InParams.sequence);
+				SubmitJob();
+			}
+			PrintDebugMessage("SubmitJobs", "End", 1);
+		}
+
+		// Implementation of abstract method (AbsractWsClient.SubmitJob()).
+		/// <summary>Submit a job to the service</summary>
+		public override void SubmitJob()
+		{
+			PrintDebugMessage("SubmitJob", "Begin", 1);
+			JobId = RunApp(Email, JobTitle, InParams);
+			if (OutputLevel > 0 || Async) Console.WriteLine(JobId);
+			// Simulate sync mode
+			if (!Async) GetResults();
+			PrintDebugMessage("SubmitJob", "End", 1);
+		}
+
+		/// <summary>Submit a job to the service</summary>
+		/// <param name="input">Structure describing the input parameters</param>
+		/// <param name="content">Structure containing the input data</param>
+		/// <returns>A string containing the job identifier</returns>
+		public string RunApp(string email, string title, InputParameters input)
+		{
+			PrintDebugMessage("RunApp", "Begin", 1);
+			PrintDebugMessage("RunApp", "email: " + email, 2);
+			PrintDebugMessage("RunApp", "title: " + title, 2);
+			PrintDebugMessage("RunApp", "input:\n" + ObjectValueToString(input), 2);
+			string jobId = null;
+			this.ServiceProxyConnect(); // Ensure we have a service proxy
+			// Submit the job
+			jobId = SrvProxy.run(email, title, input);
+			PrintDebugMessage("RunApp", "jobId: " + jobId, 2);
+			PrintDebugMessage("RunApp", "End", 1);
+			return jobId;
+		}
+
+		// Implementation of abstract method (AbsractWsClient.GetStatus()).
+		/// <summary>Get the job status</summary>
+		/// <param name="jobId">Job identifier to get the status of.</param>
+		/// <returns>A string describing the status</returns>
+		public override string GetStatus(string jobId)
+		{
+			PrintDebugMessage("GetStatus", "Begin", 1);
+			string status = "PENDING";
+			this.ServiceProxyConnect(); // Ensure we have a service proxy
+			status = SrvProxy.getStatus(jobId);
+			PrintDebugMessage("GetStatus", "status: " + status, 2);
+			PrintDebugMessage("GetStatus", "End", 1);
+			return status;
+		}
+
+		/// <summary>
+		/// Get details of the available result types/formats for a completed job. 
+		/// </summary>
+		/// <param name="jobId">Job identifier
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <returns>An array of result type descriptions.
+		/// A <see cref="wsResultType"/>
+		/// </returns>
+		public wsResultType[] GetResultTypes(string jobId)
+		{
+			PrintDebugMessage("GetResultTypes", "Begin", 2);
+			wsResultType[] resultTypes = SrvProxy.getResultTypes(jobId);
+			PrintDebugMessage("GetResultTypes", "End", 2);
+			return resultTypes;
+		}
+
+		// Implementation of abstract method (AbsractWsClient.PrintResultTypes()).
+		/// <summary>Print a summary of the result types for a job</summary>
+		public override void PrintResultTypes()
+		{
+			PrintDebugMessage("PrintResultTypes", "Begin", 1);
+			PrintDebugMessage("PrintResultTypes", "JobId: " + JobId, 2);
+			this.ServiceProxyConnect(); // Ensure we have a service proxy
+			wsResultType[] resultTypes = GetResultTypes(JobId);
+			PrintDebugMessage("PrintResultTypes", "resultTypes: " + resultTypes.Length, 2);
+			PrintProgressMessage("Getting output formats for job " + JobId, 1);
+			foreach(wsResultType resultType in resultTypes)
+			{
+				Console.WriteLine(resultType.identifier);
+				if(resultType.label != null) Console.WriteLine("\t{0}", resultType.label);
+				if(resultType.description != null) Console.WriteLine("\t{0}", resultType.description);
+				if(resultType.mediaType != null) Console.WriteLine("\t{0}", resultType.mediaType);
+				if(resultType.fileSuffix != null) Console.WriteLine("\t{0}", resultType.fileSuffix);
+			}
+			PrintDebugMessage("PrintResultTypes", "End", 1);
+		}
+
+		// Implementation of abstract method (AbsractWsClient.GetResult()).
+		public override byte[] GetResult(string jobId, string format)
+		{
+			PrintDebugMessage("GetResult", "Begin", 1);
+			PrintDebugMessage("GetResult", "jobId: " + jobId, 1);
+			PrintDebugMessage("GetResult", "format: " + format, 1);
+			byte[] result = null;
+			result = SrvProxy.getResult(jobId, format, null);
+			PrintDebugMessage("GetResult", "End", 1);
+			return result;
+		}
+
+		// Implementation of abstract method (AbsractWsClient.GetResults()).
+		/// <summary>Get the job results</summary>
+		/// <param name="jobId">Job identifier to get the results from.</param>
+		/// <param name="outformat">Selected output format or null for all formats.</param>
+		/// <param name="outFileBase">Basename for the output file. If null the jobId will be used.</param>
+		public override void GetResults(string jobId, string outformat, string outFileBase)
+		{
+			PrintDebugMessage("GetResults", "Begin", 1);
+			PrintDebugMessage("GetResults", "jobId: " + jobId, 2);
+			PrintDebugMessage("GetResults", "outformat: " + outformat, 2);
+			PrintDebugMessage("GetResults", "outFileBase: " + outFileBase, 2);
+			this.ServiceProxyConnect(); // Ensure we have a service proxy
+			// Check status, and wait if not finished
+			ClientPoll(jobId);
+			// Use JobId if output file name is not defined
+			string tmpOutFile = jobId;
+			if(outFileBase != null) tmpOutFile = outFileBase;
+			// Get list of data types
+			wsResultType[] resultTypes = GetResultTypes(jobId);
+			PrintDebugMessage("GetResults", "resultTypes: " + resultTypes.Length + " available", 2);
+			// Get the data and write it to a file
+			Byte[] res = null;
+			if (outformat != null)
+			{ // Specified data type
+				wsResultType selResultType = null;
+				foreach (wsResultType resultType in resultTypes)
+				{
+					if (resultType.identifier == outformat) selResultType = resultType;
+				}
+				PrintDebugMessage("GetResults", "resultType:\n" + ObjectValueToString(selResultType), 2);
+				res = GetResult(jobId, selResultType.identifier);
+				// Text data
+				if (selResultType.mediaType.StartsWith("text"))
+				{
+					if (tmpOutFile == "-") WriteTextFile(tmpOutFile, res);
+					else WriteTextFile(tmpOutFile + "." + selResultType.identifier + "." + selResultType.fileSuffix, res);
+				}
+				// Binary data
+				else
+				{
+					if (tmpOutFile == "-") WriteBinaryFile(tmpOutFile, res);
+					else WriteBinaryFile(tmpOutFile + "." + selResultType.identifier + "." + selResultType.fileSuffix, res);
+				}
+			}
+			else
+			{ // Data types available
+				// Write a file for each output type
+				foreach (wsResultType resultType in resultTypes)
+				{
+					PrintDebugMessage("GetResults", "resultType:\n" + ObjectValueToString(resultType), 2);
+					res = GetResult(jobId, resultType.identifier);
+					// Text data
+					if (resultType.mediaType.StartsWith("text"))
+					{
+						if (tmpOutFile == "-") WriteTextFile(tmpOutFile, res);
+						else WriteTextFile(tmpOutFile + "." + resultType.identifier + "." + resultType.fileSuffix, res);
+					}
+					// Binary data
+					else
+					{
+						if (tmpOutFile == "-") WriteBinaryFile(tmpOutFile, res);
+						else WriteBinaryFile(tmpOutFile + "." + resultType.identifier + "." + resultType.fileSuffix, res);
+					}
+				}
+			}
+			PrintDebugMessage("GetResults", "End", 1);
+		}
+
+		/// <summary>Get entry Ids from job result</summary>
+		/// <param name="jobId">Job identifer for result to get Ids from</param>
+		/// <results>List of entry Ids as a string array</results>
+		public string[] GetIds(string jobId)
+		{
+			PrintDebugMessage("GetIds", "Begin", 1);
+			PrintDebugMessage("GetIds", "jobId: " + jobId, 2);
+			string[] retVal = null;
+			this.ServiceProxyConnect(); // Ensure we have a service proxy
+			// Check status, and wait if not finished
+			ClientPoll(jobId);
+			// Get the Ids
+			byte[] content = GetResult(jobId, "ids");
+			System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+			String tempStr = enc.GetString(content);
+			char[] sepList = { '\n' };
+			retVal = tempStr.Split(sepList);
+			PrintDebugMessage("GetIds", "got " + retVal.Length + " Ids", 2);
+			PrintDebugMessage("GetIds", "End", 1);
+			return retVal;
+		}
+
+		/// <summary>Print entry Ids from job result</summary>
+		/// <param name="jobId">Job identifer for result to get Ids from</param>
+		public void PrintGetIds()
+		{
+			PrintDebugMessage("PrintGetIds", "Begin", 1);
+			PrintDebugMessage("PrintGetIds", "JobId: " + JobId, 2);
+			string[] idList = GetIds(JobId);
+			foreach (string id in idList) Console.WriteLine(id);
+			PrintDebugMessage("PrintGetIds", "End", 1);
+		}
+	}
+}
---
+++ csharp/EBIWS2_CSharp/SeqCksumCliClient/SeqCksumCliClient.cs
@@ -0,0 +1,283 @@
+/* $Id$
+ * ======================================================================
+ * 
+ * Copyright 2009-2013 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ======================================================================
+ * jDispatcher SOAP command-line client for SeqCksum
+ * ====================================================================== */
+using System;
+using System.IO;
+using EbiWS.SeqCksumWs;
+
+namespace EbiWS
+{
+	class SeqCksumCliClient : EbiWS.SeqCksumClient
+	{
+		/// <summary>Tool specific usage</summary>
+		private string usageMsg = @"SeqCksum
+========
+
+Generates checksums for a sequence using a range of checksum/digest methods.
+
+For more information see:
+- http://www.ebi.ac.uk/Tools/so/seqcksum
+- http://www.ebi.ac.uk/Tools/webservices/services/so/seqcksum_soap
+
+[Required]
+
+      --cksmethod    : str  : checksum method(s) to use, space separated. See
+                              --paramDetail cksmethod
+      --stype        : str  : input sequence type, see --paramDetail stype
+  seqFile            : file : input sequence (""-"" for STDIN, \@filename for
+                              identifier list file)
+
+[Optional]
+
+      --length       :      : report sequence length
+      --nolength     :      : do not report sequence length
+      --multifasta   :      : treat input as a set of fasta formatted sequences
+";
+
+		/// <summary>Execution entry point</summary>
+		/// <param name="args">Command-line parameters</param>
+		/// <returns>Exit status</returns>
+		public static int Main(string[] args)
+		{
+			int retVal = 0; // Return value
+			// Create an instance of the wrapper object
+			SeqCksumCliClient wsApp = new SeqCksumCliClient();
+			// If no arguments print usage and return
+			if (args.Length < 1)
+			{
+				wsApp.PrintUsageMessage();
+				return retVal;
+			}
+			try
+			{
+				// Parse the command line
+				wsApp.ParseCommand(args);
+				// Perform the selected action
+				switch (wsApp.Action)
+				{
+					case "paramList": // List parameter names
+						wsApp.PrintParams();
+						break;
+					case "paramDetail": // Parameter detail
+						wsApp.PrintParamDetail(wsApp.ParamName);
+						break;
+					case "submit": // Submit a job
+						wsApp.SubmitJobs();
+						break;
+					case "status": // Get job status
+						wsApp.PrintStatus();
+						break;
+					case "resultTypes": // Get result types
+						wsApp.PrintResultTypes();
+						break;
+					case "polljob": // Get job results
+						wsApp.GetResults();
+						break;
+					case "getids": // Get IDs from job result
+						wsApp.PrintGetIds();
+						break;
+					case "help": // Do help
+						wsApp.PrintUsageMessage();
+						break;
+					default: // Any other action.
+						Console.WriteLine("Error: unknown action " + wsApp.Action);
+						retVal = 1;
+						break;
+				}
+			}
+			catch (System.Exception ex)
+			{ // Catch all exceptions
+				Console.Error.WriteLine("Error: " + ex.Message);
+				Console.Error.WriteLine(ex.StackTrace);
+				retVal = 2;
+			}
+			return retVal;
+		}
+
+		/// <summary>
+		/// Print the usage message.
+		/// </summary>
+		private void PrintUsageMessage()
+		{
+			PrintDebugMessage("PrintUsageMessage", "Begin", 1);
+			Console.WriteLine(usageMsg);
+			PrintGenericOptsUsage();
+			PrintDebugMessage("PrintUsageMessage", "End", 1);
+		}
+
+		/// <summary>Parse command-line options</summary>
+		/// <param name="args">Command-line options</param>
+		private void ParseCommand(string[] args)
+		{
+			PrintDebugMessage("ParseCommand", "Begin", 1);
+			InParams = new InputParameters();
+			// Force default values
+			InParams.stype = "protein";
+			for (int i = 0; i < args.Length; i++)
+			{
+				PrintDebugMessage("parseCommand", "arg: " + args[i], 2);
+				switch (args[i])
+				{
+						// Generic options
+					case "--help": // Usage info
+						Action = "help";
+						break;
+					case "-h":
+						goto case "--help";
+					case "/help":
+						goto case "--help";
+					case "/h":
+						goto case "--help";
+					case "--params": // List input parameters
+						Action = "paramList";
+						break;
+					case "/params":
+						goto case "--params";
+					case "--paramDetail": // Parameter details
+						ParamName = args[++i];
+						Action = "paramDetail";
+						break;
+					case "/paramDetail":
+						goto case "--paramDetail";
+					case "--jobid": // Job Id to get status or results
+						JobId = args[++i];
+						break;
+					case "/jobid":
+						goto case "--jobid";
+					case "--status": // Get job status
+						Action = "status";
+						break;
+					case "/status":
+						goto case "--status";
+					case "--resultTypes": // Get result types
+						Action = "resultTypes";
+						break;
+					case "/resultTypes":
+						goto case "--resultTypes";
+					case "--polljob": // Get results for job
+						Action = "polljob";
+						break;
+					case "/polljob":
+						goto case "--polljob";
+					case "--outfile": // Base name for results file(s)
+						OutFile = args[++i];
+						break;
+					case "/outfile":
+						goto case "--outfile";
+					case "--outformat": // Only save results of this format
+						OutFormat = args[++i];
+						break;
+					case "/outformat":
+						goto case "--outformat";
+					case "--ids": // Get entry IDs from result
+						Action = "getids";
+						break;
+					case "/ids":
+						goto case "--ids";
+					case "--verbose": // Output level
+						OutputLevel++;
+						break;
+					case "/verbose":
+						goto case "--verbose";
+					case "--quiet": // Output level
+						OutputLevel--;
+						break;
+					case "/quiet":
+						goto case "--quiet";
+					case "--email": // User e-mail address
+						Email = args[++i];
+						break;
+					case "/email":
+						goto case "--email";
+					case "--title": // Job title
+						JobTitle = args[++i];
+						break;
+					case "/title":
+						goto case "--title";
+					case "--async": // Async submission
+						Action = "submit";
+						Async = true;
+						break;
+					case "/async":
+						goto case "--async";
+					case "--debugLevel":
+						DebugLevel = Convert.ToInt32(args[++i]);
+						break;
+					case "/debugLevel":
+						goto case "--debugLevel";
+					case "--endpoint": // Service endpoint
+						ServiceEndPoint = args[++i];
+						break;
+					case "/endpoint":
+						goto case "--endpoint";
+					case "--multifasta":
+						this.multifasta = true;
+						break;
+					case "/multifasta":
+						goto case "--multifasta";
+
+						// Tool specific options
+					case "--cksmethod": // Checksum methods.
+						char[] sepList = { ' ', ',' };
+						InParams.cksmethod = args[++i].Split(sepList);
+						Action = "submit";
+						break;
+					case "/cksmethod":
+						goto case "--cksmethod";
+					case "--stype": // Input sequence type
+						InParams.stype = args[++i];
+						break;
+					case "/stype":
+						goto case "--stype";
+					case "--length": // Report sequence length
+						InParams.length = true;
+						InParams.lengthSpecified = true;
+						break;
+					case "/length":
+						goto case "--length";
+					case "--nolength": // Do not report sequence length
+						InParams.length = false;
+						InParams.lengthSpecified = true;
+						break;
+					case "/nolength":
+						goto case "--nolength";
+
+						// Input data/sequence option
+					case "--sequence": // Input sequence
+						i++;
+						goto default;
+					default:
+						// Check for unknown option
+						if (args[i].StartsWith("--") || args[i].LastIndexOf('/') == 0)
+						{
+							Console.Error.WriteLine("Error: unknown option: " + args[i] + "\n");
+							Action = "exit";
+							return;
+						}
+						// Must be data argument
+						InParams.sequence = args[i];
+						Action = "submit";
+						break;
+				}
+			}
+			PrintDebugMessage("ParseCommand", "End", 1);
+		}
+	}
+}
