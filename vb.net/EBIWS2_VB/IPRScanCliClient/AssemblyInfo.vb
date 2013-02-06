9a7597ab63d484732d0aee7530a25db9871b1c15
hpm <hpm@mycompany.com>
2013-02-06 13:50:59 +0000

    Add assembly information.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@2522 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

--- vb.net/EBIWS2_VB/IPRScanCliClient/AssemblyInfo.vb
+++ vb.net/EBIWS2_VB/IPRScanCliClient/AssemblyInfo.vb
@@ -1,3 +1,6 @@
+' $Id$
+' ======================================================================
+
 Imports System.Reflection
 Imports System.Runtime.CompilerServices
 
@@ -5,21 +8,21 @@ Imports System.Runtime.CompilerServices
 ' Change them to the values specific to your project.
 
 <assembly: AssemblyTitle("IPRScanCliClient")>
-<assembly: AssemblyDescription("")>
+<assembly: AssemblyDescription("Sample command-line client for the InterProScan (SOAP) Web Service")>
 <assembly: AssemblyConfiguration("")>
-<assembly: AssemblyCompany("")>
-<assembly: AssemblyProduct("")>
-<assembly: AssemblyCopyright("")>
-<assembly: AssemblyTrademark("")>
+<assembly: AssemblyCompany("EMBL - European Bioinformatics Institute")>
+<assembly: AssemblyProduct("EMBL-EBI Web Services Sample Clients")>
+<assembly: AssemblyCopyright("Copyright © 2011-2013 EMBL - European Bioinformatics Institute")>
+'<assembly: AssemblyTrademark("")>
 <assembly: AssemblyCulture("")>
 
 ' The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 ' If the build and revision are set to '*' they will be updated automatically.
 
-<assembly: AssemblyVersion("1.0.0.0")>
+<assembly: AssemblyVersion("1.0.*")>
 
 ' The following attributes are used to specify the signing key for the assembly, 
 ' if desired. See the Mono documentation for more information about signing.
 
-<assembly: AssemblyDelaySign(false)>
-<assembly: AssemblyKeyFile("")>
+'<assembly: AssemblyDelaySign(false)>
+'<assembly: AssemblyKeyFile("")>
--- vb.net/EBIWS2_VB/NcbiBlastCliClient/AssemblyInfo.vb
+++ vb.net/EBIWS2_VB/NcbiBlastCliClient/AssemblyInfo.vb
@@ -1,3 +1,22 @@
+' $Id$
+' ======================================================================
+' 
+' Copyright 2011-2013 EMBL - European Bioinformatics Institute
+'
+' Licensed under the Apache License, Version 2.0 (the "License");
+' you may not use this file except in compliance with the License.
+' You may obtain a copy of the License at
+'
+'     http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law or agreed to in writing, software
+' distributed under the License is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+' See the License for the specific language governing permissions and
+' limitations under the License.
+' 
+' ======================================================================
+
 Imports System.Reflection
 Imports System.Runtime.CompilerServices
 
@@ -5,21 +24,21 @@ Imports System.Runtime.CompilerServices
 ' Change them to the values specific to your project.
 
 <assembly: AssemblyTitle("NcbiBlastCliClient")>
-<assembly: AssemblyDescription("")>
+<assembly: AssemblyDescription("Sample command-line client for the NCBI BLAST (SOAP) Web Service")>
 <assembly: AssemblyConfiguration("")>
-<assembly: AssemblyCompany("")>
-<assembly: AssemblyProduct("")>
-<assembly: AssemblyCopyright("")>
-<assembly: AssemblyTrademark("")>
+<assembly: AssemblyCompany("EMBL - European Bioinformatics Institute")>
+<assembly: AssemblyProduct("EMBL-EBI Web Services Sample Clients")>
+<assembly: AssemblyCopyright("Copyright © 2011-2013 EMBL - European Bioinformatics Institute")>
+'<assembly: AssemblyTrademark("")>
 <assembly: AssemblyCulture("")>
 
 ' The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 ' If the build and revision are set to '*' they will be updated automatically.
 
-<assembly: AssemblyVersion("1.0.0.0")>
+<assembly: AssemblyVersion("1.0.*")>
 
 ' The following attributes are used to specify the signing key for the assembly, 
 ' if desired. See the Mono documentation for more information about signing.
 
-<assembly: AssemblyDelaySign(false)>
-<assembly: AssemblyKeyFile("")>
+'<assembly: AssemblyDelaySign(false)>
+'<assembly: AssemblyKeyFile("")>
