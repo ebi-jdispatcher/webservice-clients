71a86641f7bd18d0d362c89f25a2db6d8e5a0e15
hpm <hpm@mycompany.com>
2014-04-11 10:59:20 +0000

    Sample InterProScan 5 (SOAP) for PHP SOAP.
    git-svn-id: https://svn.ebi.ac.uk/webservices/webservices-2.0/trunk@2770 8a1ee80d-b71c-0410-a81b-92656cd9ddcf

---
+++ php/php_soap/iprscan5_lib_php_soap.php
@@ -0,0 +1,184 @@
+<?php
+# $Id$
+# ======================================================================
+# 
+# Copyright 2008-2014 EMBL - European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# ======================================================================
+# PHP InterProScan 5 (SOAP) client library
+#
+# Tested with:
+#   PHP 5.1.6 (CentOS 5)
+#   PHP 5.3.2 (Ubuntu 10.04 LTS)
+#   PHP 5.3.3 (CentOS 6)
+#   PHP 5.3.10 (Ubuntu 12.04 LTS)
+#
+# See:
+# http://www.ebi.ac.uk/Tools/webservices/services/pfa/iprscan5_soap
+# http://www.ebi.ac.uk/Tools/webservices/tutorials/php
+# ======================================================================
+
+class IPRScan5Client {
+  // Service WSDL
+  private $wsdlUrl = 'http://www.ebi.ac.uk/Tools/services/soap/iprscan5?wsdl';
+  // Service proxy
+  private $srvProxy;
+  // HTTP proxy details
+  private $httpProxy;
+  // Trace flag
+  public $trace = FALSE;
+  // Debug level
+  public $debugLevel = 0;
+
+  // Debug message
+  protected function printDebugMessage($method, $message, $level) {
+    if($level <= $this->debugLevel) {
+      // Plain text
+      if(array_key_exists('argc', $GLOBALS)) print "[$method] $message\n";
+      // HTML
+      else print "<p>[$method] $message</p>\n";
+    }
+  }
+
+  // Set HTTP proxy details
+  function setHttpProxy($host, $port=8080) {
+    $this->printDebugMessage('setHttpProxy', 'Begin', 1);
+    $this->httpProxy = array('proxy_host' => $host,
+			     'proxy_port' => $port);
+    $this->printDebugMessage('setHttpProxy', 'End', 1);
+  }
+
+  // Get HTTP User-agent string.
+  function getUserAgent() {
+    $this->printDebugMessage('getUserAgent', 'Begin', 2);
+    $clientVersion = trim(substr('$Revision$', 11), ' $');
+    if($clientVersion == '') $clientVersion = '0';
+    $uname  = posix_uname();
+    $userAgent = 'EBI-Sample-Client/' . $clientVersion . 
+      ' (' . get_class($this) . '; ' . $uname['sysname'] . ') PHP-SOAP/' . phpversion();
+    $this->printDebugMessage('getUserAgent', 'User-agent: ' . $userAgent, 2);
+    $this->printDebugMessage('getUserAgent', 'End', 2);
+    return $userAgent;
+  }
+  
+  // Set WSDL to an alternative server
+  function setWsdlUrl($wsdlUrl) {
+    $this->printDebugMessage('setWsdlUrl', 'Begin', 1);
+    $this->printDebugMessage('setWsdlUrl', 'wsdlUrl: ' . $wsdlUrl, 2);
+    $this->wsdlUrl = $wsdlUrl;
+    $this->printDebugMessage('setWsdlUrl', 'End', 1);
+  }
+
+  // Get a service proxy
+  function serviceProxyConnect() {
+    $this->printDebugMessage('serviceProxyConnect', 'Begin', 3);
+    // Check for SoapClient
+    if(!class_exists('SoapClient')) {
+      throw new Exception('SoapClient class cannot be found.');
+    }
+    // Get service proxy
+    if($this->srvProxy == null) {
+      $options = array('trace' => $this->trace,
+		       'user_agent' => $this->getUserAgent()
+		       );
+      if(isset($this->httpProxy)) {
+	$options['proxy_host'] = $this->httpProxy['proxy_host'];
+	$options['proxy_port'] = $this->httpProxy['proxy_port'];
+      }
+      $this->srvProxy = new SoapClient($this->wsdlUrl,
+				    $options);
+    }
+    $this->printDebugMessage('serviceProxyConnect', 'End', 3);
+  }
+
+  // SOAP message trace output.
+  function soapTrace() {
+    $this->printDebugMessage('soapTrace', 'Begin', 12);
+    echo "REQUEST:\n" . $this->srvProxy->__getLastRequest() . "\n";
+    echo "RESPONSE:\n" . $this->srvProxy->__getLastResponse() . "\n";
+    $this->printDebugMessage('soapTrace', 'End', 12);
+  }
+
+  // Get list of tool parameter names
+  function getParameters() {
+    $this->printDebugMessage('getParameters', 'Begin', 1);
+    $this->serviceProxyConnect();
+    $parameters = $this->srvProxy->getParameters();
+    if($this->trace) $this->soapTrace();
+    $this->printDebugMessage('getParameters', 'End', 1);
+    return $parameters->parameters->id;
+  }
+
+  // Get detail of a parameter
+  function getParameterDetails($parameterId) {
+    $this->printDebugMessage('getParameterDetails', 'Begin', 1);
+    $this->printDebugMessage('getParameterDetails',
+			     "parameterId: $parameterId", 2);
+    $this->serviceProxyConnect();
+    $res = $this->srvProxy->getParameterDetails(array('parameterId' => $parameterId));
+    if($this->trace) $this->soapTrace();
+    $this->printDebugMessage('getParameterDetails', 'End', 1);
+    return $res->parameterDetails;
+  }
+
+  // Submit a job to the service
+  function run($email, $title, $params) {
+    $this->printDebugMessage('run', 'Begin', 1);
+    $this->serviceProxyConnect();
+    $res = $this->srvProxy->run(
+			     array('email' => $email,
+				   'title' => $title,
+				   'parameters' => $params)
+			     );
+    if($this->trace) $this->soapTrace();
+    $this->printDebugMessage('run', 'End', 1);
+    return $res->jobId;
+  }
+
+  // Get the status of a job
+  function getStatus($jobId) {
+    $this->printDebugMessage('getStatus', 'Begin', 1);
+    $this->serviceProxyConnect();
+    $res = $this->srvProxy->getStatus(array('jobId' => $jobId));
+    if($this->trace) $this->soapTrace();
+    $this->printDebugMessage('getStatus', 'End', 1);
+    return $res->status;
+  }
+  
+  // Available result types for a finished job
+  function getResultTypes($jobId) {
+    $this->printDebugMessage('getResultTypes', 'Begin', 1);
+    $this->serviceProxyConnect();
+    $res = $this->srvProxy->getResultTypes(array('jobId' => $jobId));
+    if($this->trace) $this->soapTrace();
+    $this->printDebugMessage('getResultTypes', 'End', 1);
+    return $res->resultTypes->type;
+  }
+  
+  // Get job results
+  function getResult($jobId, $type) {
+    $this->printDebugMessage('getResult', 'Begin', 1);
+    $this->serviceProxyConnect();
+    $res = $this->srvProxy->getResult(array('jobId' => $jobId,
+					    'type' => $type,
+					    'parameters' => array()
+					    )
+				      );
+    if($this->trace) $this->soapTrace();
+    $this->printDebugMessage('getResult', 'End', 1);
+    return $res->output;
+  }
+}
+?>
---
+++ php/php_soap/iprscan5_web_php_soap.php
@@ -0,0 +1,348 @@
+<html>
+<!-- $Id$ -->
+<body>
+<?php
+# ======================================================================
+# 
+# Copyright 2009-2014 EMBL - European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# ======================================================================
+# PHP InterProScan 5 (SOAP) web client
+#
+# Tested with:
+#   PHP 5.1.6 (CentOS 5)
+#   PHP 5.3.2 (Ubuntu 10.04 LTS)
+#   PHP 5.3.3 (CentOS 6)
+#   PHP 5.3.10 (Ubuntu 12.04 LTS)
+#
+# See:
+# http://www.ebi.ac.uk/Tools/webservices/services/sss/iprscan5_soap
+# http://www.ebi.ac.uk/Tools/webservices/services/sss/iprscan5_rest
+# http://www.ebi.ac.uk/Tools/webservices/tutorials/php
+# ======================================================================
+// Load library
+require_once('iprscan5_lib_php_soap.php');
+
+// Extend client class
+class IPRScan5WebClient extends IPRScan5Client {
+  // Print list of tool parameters
+  function printGetParameters() {
+    $this->printDebugMessage('printGetParameters', 'Begin', 1);
+    $paramList = $this->getParameters();
+    print "<ul>\n";
+    foreach($paramList as $paramName) {
+      print '<li>' . $paramName . "</li>\n";
+    }
+    print "</ul>\n";
+    $this->printDebugMessage('printGetParameters', 'End', 1);
+  }
+
+  // Print details of a parameter
+  function printGetParameterDetails($parameterId) {
+    $this->printDebugMessage('printGetParameterDetails', 'Begin', 1);
+    $paramDetail = $this->getParameterDetails($parameterId);
+    print <<<EOF
+<h3>$paramDetail->name</h3>
+
+<p>$paramDetail->description</p>
+EOF
+    ;
+    if(isset($paramDetail->values)) {
+      print "<table border=\"1\">\n";
+      print "<tr><th>Label</th><th>Value</th><th>Default</th></tr>\n";
+      foreach($paramDetail->values->value as $val) {
+	print "<tr><td>$val->label</td><td>$val->value</td><td>";
+	if($val->defaultValue) print 'default';
+	else print '&nbsp;';
+	print "</td></tr>\n";
+      }
+      print "</table>\n";
+    }
+    $this->printDebugMessage('printGetParameterDetails', 'Begin', 1);
+  }
+  
+  // Label for an option, generated from the parameter details.
+  function paramDetailToLabelStr($parameterId, $paramDetail) {
+    $this->printDebugMessage('paramDetailToLabelStr', 'Begin', 1);
+    $helpUrl = '?paramDetail=' . $parameterId;
+    $retStr = '<a href="' . $helpUrl . '">' . $paramDetail->name .'</a>: ';
+    $this->printDebugMessage('paramDetailToLabelStr', 'End', 1);
+    return $retStr;
+  }
+  
+  // Generate HTML option tags for a parameter detail.
+  function paramDetailToOptionStr($paramDetail) {
+    $this->printDebugMessage('paramDetailToOptionStr', 'Begin', 1);
+    $retStr = '';
+    if(isset($paramDetail->values)) {
+      foreach($paramDetail->values->value as $val) {
+	if($val->defaultValue) {
+	  $retStr .= "<option selected=\"1\" value=\"$val->value\">$val->label</option>\n";
+	}
+	else {
+	  $retStr .= "<option value=\"$val->value\">$val->label</option>\n";
+	}
+      }
+    }
+    $this->printDebugMessage('paramDetailToOptionStr', 'End', 1);
+    return $retStr;
+  }
+
+  // Generate HTML tags for a parameter detail. menu or text input.
+  function paramDetailToStr($parameterId, $multi=FALSE) {
+    $this->printDebugMessage('paramDetailToStr', 'Begin', 1);
+    $paramDetail = $this->getParameterDetails($parameterId);
+    $retStr = $this->paramDetailToLabelStr($parameterId, $paramDetail);
+    if(isset($paramDetail->values)) {
+      if($multi) {
+	// Multi-select menu
+	$retStr .= '<select name="' . $parameterId .'[]"  multiple="1">';
+      }
+      else {
+	// Drop-down list
+	$retStr .= '<select name="' . $parameterId .'">';
+      }
+      $retStr .= $this->paramDetailToOptionStr($paramDetail);
+      $retStr .= '</select>';
+    }
+    else {
+      // Input box
+      $retStr .= '<input type="text" name="' . $parameterId . '" />';
+    }
+    $this->printDebugMessage('paramDetailToStr', 'End', 1);
+    return $retStr;
+  }
+
+  // Output a submission form
+  function printForm() {
+    $this->printDebugMessage('printForm', 'Begin', 1);
+    $databaseStr =  $this->paramDetailToStr('appl', TRUE);
+    
+    print <<<EOF
+<form method="POST">
+<p>E-mail: <input type="text" name="email" />&nbsp;
+Job title: <input type="text" name="title" /></p>
+
+<p>
+<a href="?paramDetail=sequence">Sequence</a>:<br />
+<textarea name="sequence" rows="5" cols="80">
+</textarea></p>
+
+<p>$databaseStr</p>
+
+<p align="right">
+<input type="submit" value="Submit" />
+<input type="reset" value="Reset" />
+</p>
+</form>
+EOF
+  ;
+    $this->printDebugMessage('printForm', 'End', 1);
+  }
+  
+  // Submit a job to the service.
+  function submitJob($options) {
+    $this->printDebugMessage('submitJob', 'Begin', 1);
+    $params  = array();
+    foreach($options as $key => $val) {
+      switch($key) {
+      case 'appl':
+	$params[$key] = $val;
+	break;
+      case 'sequence':
+	$params[$key] = $val;
+	break;
+      }
+    }
+    $jobId = $this->run(
+			$options['email'],
+			$options['title'],
+			$params
+			);
+    echo "<p>Job Id: <a href=\"?jobId=$jobId\">$jobId</a></p>";
+    echo "<p>Please wait...</p>";
+    $this->printDebugMessage('submitJob', 'End', 1);
+    return $this->genMetaRefresh($jobId);
+  }
+
+  // Get the status of a job.
+  function printStatus($jobId) {
+    $this->printDebugMessage('printStatus', 'Begin', 1);
+    $retVal = '';
+    $status = $this->getStatus($jobId);
+    echo "<p>Status for job <a href=\"?jobId=$jobId\">$jobId</a>: $status</p>\n";
+    if($status == 'FINISHED') {
+      $this->printResultsSummary($jobId);
+    }
+    else {
+      $retVal = $this->genMetaRefresh($jobId);
+    }
+    $this->printDebugMessage('printStatus', 'End', 1);
+    return $retVal;
+  }
+
+  // Print details of available results for a job
+  function printResultsSummary($jobId) {
+    $this->printDebugMessage('printResultsSummary', 'Begin', 1);
+    echo "<p>Results:</p>\n";
+    echo "<ul>\n";
+    $resultTypes = $this->getResultTypes($jobId);
+    foreach($resultTypes as $resultType) {
+      $resultUrl = "?jobId=$jobId&resultType=" . $resultType->identifier;
+      print "<li><a href=\"$resultUrl\">" . $resultType->label . "</a>";
+      if(isset($resultType->description)) {
+	print ": " . $resultType->description . "\n";
+      }
+      print "</li>\n";
+    }
+    echo "</ul>\n";
+    $this->printDebugMessage('printResultsSummary', 'End', 1);
+  }
+
+  // Get a job result.
+  function printResult($jobId, $resultType) {
+    $this->printDebugMessage('printResult', 'Begin', 1);
+    echo "<p>Result for job <a href=\"?jobId=$jobId\">$jobId</a>:</p>\n";
+    $resultTypeObjs = $this->getResultTypes($jobId);
+    foreach($resultTypeObjs as $resultTypeObj) {
+      if($resultTypeObj->identifier == $resultType) {
+	$selResultTypeObj = $resultTypeObj;
+      }
+    }
+    // Plain text
+    if($selResultTypeObj->mediaType == 'text/plain') {
+      $resultStr = $this->getResult($jobId, $resultType);
+      echo "<p><pre>$resultStr</pre></p>\n";
+    }
+    // Image, embed using img tag using service REST API for document
+    elseif(strpos($selResultTypeObj->mediaType, 'image') === 0 &&
+	   strpos($selResultTypeObj->mediaType, 'xml') == 0) {
+      $resultUrl = 'http://www.ebi.ac.uk/Tools/services/rest/iprscan5/result/';
+      $resultUrl .= $jobId . '/' . $resultType;
+      echo "<img src=\"$resultUrl\"></img>";
+    }
+    // Other, embed in iframe using service REST API for document
+    else {
+      $resultUrl = 'http://www.ebi.ac.uk/Tools/services/rest/iprscan5/result/';
+      $resultUrl .= $jobId . '/' . $resultType;
+      echo "<iframe src=\"$resultUrl\" width=\"100%\" height=\"100%\"></iframe>";
+    }
+    $this->printDebugMessage('printResult', 'Begin', 1);
+  }
+
+  // Generate meta-refresh tag for a job.
+  function genMetaRefresh($jobId) {
+    $this->printDebugMessage('genMetaRefresh', 'Begin', 2);
+    $statusUrl = "?jobId=$jobId";
+    $retVal = "<meta http-equiv=\"refresh\" content=\"10;url=$statusUrl\">";
+    $this->printDebugMessage('genMetaRefresh', 'Begin', 2);
+    return $retVal;
+  }
+}
+?>
+
+<h1 align="center">InterProScan 5 (SOAP)</h1>
+<hr />
+
+<?php
+// No refresh.
+$metaRefresh = '';
+// Check PHP version...
+if(floatval(phpversion()) < 5.0) {
+  echo "<p>PHP 5 is required for this page. This is PHP " . phpversion() . "</p>";
+}
+else {
+
+  // Map PHP errors to exceptions
+  if(class_exists('ErrorException')) {
+    function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+      throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+    set_error_handler("exception_error_handler");
+  }
+  
+  try {
+    // Grab input params
+    $inputParams = array();
+    if(isset($_POST) && count($_POST) > 0) {
+      $inputParams = $_POST;
+    }
+    elseif(isset($_GET) && count($_GET) > 0) {
+      $inputParams = $_GET;
+    }
+
+    // Create an instance of the client.
+    $client = new IPRScan5WebClient();
+    // HTTP proxy config.
+    //$client->setHttpProxy($proxy_host, $proxy_port);
+
+    // Debug
+    if(array_key_exists('debug', $inputParams)) $client->debugLevel = 2;
+
+    // Get a result
+    if(array_key_exists('jobId', $inputParams) &&
+       array_key_exists('resultType', $inputParams)) {
+      $client->printResult($inputParams['jobId'],
+			   $inputParams['resultType']);
+    }
+    // Get job status, and poll
+    elseif(array_key_exists('jobId', $inputParams)) {
+      $metaRefresh = $client->printStatus($inputParams['jobId']);
+    }
+    // Option/parameter details/help.
+    elseif(array_key_exists('paramDetail', $inputParams)) {
+      $client->printGetParameterDetails($inputParams['paramDetail']);
+    }
+    // Submit a job
+    elseif(array_key_exists('sequence', $inputParams)) {
+      $metaRefresh = $client->submitJob($inputParams);
+    }
+    // Input form
+    else {
+      $client->printForm();
+    }
+  }
+  catch(SoapFault $ex) {
+    echo '<p><b>Error</b>: ';
+    if($ex->getMessage() != '') echo $ex->getMessage();
+    else echo $ex;
+    echo "</p>\n";
+    // In debug mode, output message trace for exception.
+    if(array_key_exists('debug', $inputParams)) {
+      print '<p><pre>';
+      $client->soapTrace();
+      print '</pre></p>';
+    }
+  }
+  catch(Exception $ex) {
+    echo '<p><b>Error</b>: ';
+    if($ex->getMessage() != '') echo $ex->getMessage();
+    else echo $ex;
+    echo "</p>\n";
+  }
+}
+?>
+<hr />
+<p>Powered by <a href="http://www.ebi.ac.uk/Tools/webservices/">EMBL-EBI Web Services</a></p>
+</body>
+
+<head>
+<title>InterProScan 5 (SOAP)</title>
+</head>
+<?php
+// Meta refresh
+print $metaRefresh;
+?>
+</html>
